71494d4e10c5056805d38aa1017867ffc9b69212
errpenk <errpenk@gmail.com>
2026-03-26 11:39:32 +0100

    Create config.py

---
+++ VADER/VADER_Script/config.py
@@ -0,0 +1,9 @@
+INPUT_FILE   = "your_file.csv"    # file route
+TEXT_COLUMN  = "content"           # column
+OUTPUT_FILE  = "vader_results.xlsx"
+
+# Translation speed is limited
+TRANSLATE_DELAY = 0.5 # 0.3-1.0
+
+
+# MAX_RETRIES = 3
