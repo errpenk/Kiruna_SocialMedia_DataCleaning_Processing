0270b021179b439e0022d81ee624310d525035b9
errpenk <errpenk@gmail.com>
2026-03-26 17:05:52 +0100

    Update core use.py

--- VADER/core use.py
+++ VADER/core use.py
@@ -3,7 +3,7 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 analyzer = SentimentIntensityAnalyzer()
 
-text   = "Kiruna's relocation is an incredible feat of urban planning!"
+text = "Kiruna's relocation is an incredible feat of urban planning!"
 result = analyzer.polarity_scores(text)
 
 print(f"Text   : {text}")
